aaab21662a522fe3be21df56ed4088ef142b7702
Lucas Cepeda <lucas.cepeda2@gmail.com>
2023-08-17 22:43:05 -0300

    Validaciones y estilos agregados

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,20 +2,19 @@
 //que envia el usuario
 
 function Card({usuarios}) {
-  return (
-      <div>
-      <h2>Usuarios</h2>
-      {usuarios.map((usuario,index)=>(
-          <div key={index}>
-          <h3>{usuario.nombre} {usuario.piloto}</h3>
-          </div>
-      )
-          
-      )
-      }
-      </div>
-  )
+    return (
+        <div>
+        <h2>Respuestas</h2>
+        {usuarios.map((usuario,index)=>(
+            <div key={index}>
+            <h3> Hola {usuario.nombre}!, tu piloto favorito es: {usuario.piloto}</h3>
+            </div>
+        )  
+        )
+        }
+        </div>
+    )
 
-}
-
-export default Card;
+  }
+  
+  export default Card; 
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,25 +1,47 @@
 import { useState } from "react"
 
-export default function Form({onAddUsuario}){
-    const [nombre,setNombre] = useState("");
-    const [piloto,setPiloto] = useState("");
+export default function Form({ onAddUsuario }) {
+    const [piloto, setPiloto] = useState("");
+    const [nombre, setNombre] = useState("");
 
-    const onHandleSubmit= (e)=>{
+    
+    const validarPiloto = (piloto) => {
+        if (piloto.length > 5) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+    const validarNombre = (nombre) => {
+        const sinEspacios = nombre.trim();
+        if (sinEspacios.length > 2) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+
+    const onHandleSubmit = (e) => {
         e.preventDefault();
-        if(nombre && piloto){
-            onAddUsuario({nombre,piloto})
+        const nombreValido = validarNombre(nombre);
+        const pilotoValido = validarPiloto(piloto);
+        if (!nombreValido || !pilotoValido) {
+            alert("Por favor, revisá los datos cargados. Recordá que tu nombre tiene que ser mayor a 2 caracteres y el nombre de tu piloto debe contener 6 caracteres");
+        } else {
+            onAddUsuario({ nombre, piloto })
             setNombre("")
             setPiloto("")
         }
     }
-    
 
-    return(
-            <form onSubmit={onHandleSubmit}>
-                <input type="text" placeholder="Nombre" value={nombre} onChange={(e)=>setNombre(e.target.value)} />
-                <input type="text" placeholder="Piloto favorito" value={piloto} onChange={(e)=>setPiloto(e.target.value)} />
-                <button type="submit">Enviar</button>
-            </form>
+
+    return (
+        <form onSubmit={onHandleSubmit}>
+            <input type="text" placeholder="Nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+            <input type="text" placeholder="Piloto favorito" value={piloto} onChange={(e) => setPiloto(e.target.value)} />
+            <button type="submit">Enviar</button>
+        </form>
     )
 
 }
